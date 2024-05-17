74e91e50cb24f06f766d95f981f1ae046fcad34e
Pierre Sourice <pierre.sourice38@gmail.com>
2024-05-17 10:24:14 +0200

    Réorganisaiton routes

--- frontend/src/appRouter.jsx
+++ frontend/src/appRouter.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import HomePage from "./homePage";
-import "../assets/css/style.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/homePage";
+import "./assets/css/style.css";
 
 export default function AppRouter() {
 	return (
